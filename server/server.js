ae8c681bc3bbd0091e19280eb9ea7319828e7fd1
Soufiane ALOUI EL IDRISSI <tanger.soufian2015@gmail.com>
2024-07-10 17:25:52 +0100

    Handel the sql query of creating inscription database

--- server/server.js
+++ server/server.js
@@ -1,44 +1,60 @@
 require("dotenv").config({ path: __dirname + "/.env" }); // import .env file
 const express = require("express");
 const http = require("http"); // import web client-server communication protocol
-const asyncHandler = require("express-async-handler"); // middleware for handling exceptions 
-const sqlite3 = require('sqlite3').verbose();
-const portServer = process.env.PORT || 4000;  // get the number of the port from .env file
+const asyncHandler = require("express-async-handler"); // middleware for handling exceptions
+const sqlite3 = require("sqlite3").verbose();
+const portServer = process.env.PORT || 4000; // get the number of the port from .env file
 const app = express();
 const inscriptionRoutes = require("./routes/inscription.route.js");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 // Connect to  my SQLite database
 const db = new sqlite3.Database(process.env.DB_URL, (err) => {
-    if (err) {
-      console.error('Error in connecting to the database, please try again later', err);
-      return;
-    }
-    console.log('Connecting to my SQLite database');
-  });
+  if (err) {
+    console.error(
+      "Error in connecting to the database, please try again later",
+      err
+    );
+    return;
+  }
+  console.log("Connecting to my SQLite database");
+});
 
-  // Create a test table
+// Create a test table
 db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS users (
+  db.run(`CREATE TABLE IF NOT EXISTS inscriptions (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       firstName TEXT NOT NULL,
-      lastName TEXT NOT NULL
+      lastName TEXT NOT NULL,
+      birthdayDate TEXT NOT NULL,
+      birthdayPlace TEXT NOT NULL,
+      email TEXT NOT NULL,
+      phoneNumber TEXT NOT NULL,
+      postalCode TEXT NOT NULL,
+      city TEXT NOT NULL,
+      profession TEXT NOT NULL,
+      familyStatus TEXT NOT NULL,
+      circulationDate TEXT NOT NULL,
+      model TEXT NOT NULL,
+      marque TEXT NOT NULL,
+      registration TEXT NOT NULL
+      
     )`);
-  });
+});
 
-  // Attach db to request middleware
+// Attach db to request middleware
 app.use((req, res, next) => {
-    req.db = db;
-    next();
-  });
+  req.db = db;
+  next();
+});
 
-  // Import inscription route
-app.use('/api/inscriptions', inscriptionRoutes);
+// Import inscription route
+app.use("/api/inscriptions", inscriptionRoutes);
 
-const server = http.createServer(app);  // create a server 
-const startServer = asyncHandler(async () => {   // start the server
+const server = http.createServer(app); // create a server
+const startServer = asyncHandler(async () => {
+  // start the server
   server.listen(portServer, () => {
     console.log(`server start on port ${portServer}`);
   });
